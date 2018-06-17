45ef95a1eaf4378c14254116bfd68349d53e7b1d
Chris King <amznkin@amazon.com>
2018-06-17 14:22:24 +0100

    Directors doesn't have a slot, so removed the code to check for it

--- directors/lambda_function.py
+++ directors/lambda_function.py
@@ -102,7 +102,6 @@ def lambda_handler(event, context):
     alexa_event = json.loads(event['Records'][0]['Sns']['Message'])
     print(alexa_event)
     intent = alexa_event['request']['intent']
-    slot = alexa_event['request']['intent']['slots']
     if 'emailDirectors' in intent['name']:
         directors = getDirectors()
         directors = directors.to_dict()[2]
